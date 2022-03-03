034984e44dfb532a7e931dc009d5964abc50f584
mirae0312 <ekdtls58@gmail.com>
2022-03-03 21:26:44 +0900

    commit

---
+++ src/main/java/com/project/market/HomeController.java
@@ -0,0 +1,16 @@
+package com.project.market;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "forward:/index.jsp";
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>우왕 인덱스다~</h1>
+</body>
+</html>
