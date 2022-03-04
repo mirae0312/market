7c670f6e0641e452ddb52638e2edab8f9ae3f516
wkrud <jae6140@naver.com>
2022-03-05 02:20:51 +0900

    카카오(로그인, 회원가입_기반)

---
+++ src/main/java/com/project/market/security/model/dao/LoginDao.java
@@ -0,0 +1,11 @@
+package com.project.market.security.model.dao;
+
+import com.project.market.security.model.vo.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
+public interface LoginDao {
+    Member selectOneMemberById(Map<String, Object> kakaoUser);
+}
