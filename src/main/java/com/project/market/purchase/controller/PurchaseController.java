f0d2aea43d0535584743b14b2d016902a90b9198
tarr4h <tarr4h@gmail.com>
2022-03-05 21:28:51 +0900

    작업

---
+++ src/main/java/com/project/market/purchase/controller/PurchaseController.java
@@ -0,0 +1,51 @@
+package com.project.market.purchase.controller;
+
+import java.beans.PropertyEditor;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.market.purchase.model.service.PurchaseService;
+import com.project.market.purchase.model.vo.Coupon;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/purchase")
+public class PurchaseController {
+	
+	@Autowired
+	private PurchaseService purchaseService;
+	
+	
+	@GetMapping("/tarr4hMain")
+	public void tarr4hMain() {
+		
+	}
+	
+	@GetMapping("/createCoupon")
+	public ResponseEntity<?> createCoupon(Coupon coupon) {
+		int result = purchaseService.insertCoupon(coupon);
+		log.debug("insertCouponResult = {}", result);
+		
+		return ResponseEntity.ok(coupon.getCode());
+	}
+	
+	
+	@InitBinder
+    public void initBinder(WebDataBinder binder) {
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    	PropertyEditor editor = new CustomDateEditor(sdf, true);
+    	binder.registerCustomEditor(Date.class, editor);
+    }
+	
+}
---
+++ src/main/java/com/project/market/purchase/model/service/PurchaseService.java
@@ -0,0 +1,9 @@
+package com.project.market.purchase.model.service;
+
+import com.project.market.purchase.model.vo.Coupon;
+
+public interface PurchaseService {
+
+	int insertCoupon(Coupon coupon);
+
+}
