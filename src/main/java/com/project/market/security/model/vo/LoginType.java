261e6cc046e05593c91ffb60aa2fb7e296a6756c
wkrud <jae6140@naver.com>
2022-03-11 20:32:08 +0900

    공지 타입 추가

---
+++ src/main/java/com/project/market/security/model/vo/LoginType.java
@@ -0,0 +1,14 @@
+package com.project.market.security.model.vo;
+
+public enum LoginType {
+    D, G, K, N
+
+//    private String loginType;
+//    LoginType(String loginType) {
+//        this.loginType = loginType;
+//    }
+//
+//    public static LoginType _valueOf(String loginType){
+//
+//    }
+}
