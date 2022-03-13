eb1f8a7dc12d9e158c1ce36047db4071a534ef39
wkrud <jae6140@naver.com>
2022-03-14 02:09:14 +0900

    문의 crud

--- src/main/java/com/project/market/customerService/model/dao/CustomerServiceDao.java
+++ src/main/java/com/project/market/customerService/model/dao/CustomerServiceDao.java
@@ -2,7 +2,9 @@ package com.project.market.customerService.model.dao;
 
 import com.project.market.common.vo.Attachment;
 import com.project.market.customerService.model.vo.Announcement;
-import org.apache.ibatis.annotations.Mapper;
+import com.project.market.customerService.model.vo.Question;
+import com.project.market.security.model.vo.Member;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
@@ -33,4 +35,22 @@ public interface CustomerServiceDao {
 	Map<String, Object> selectUserDefaultAddress(String userId);
 
 	int selectUserAccumulationRate(String userId);
+
+    @Select("select * from mk_question where writer = #{id}")
+    List<Question> selectAllMyQuestion(Member member, RowBounds rowBounds);
+
+    @Select("select count(*) from mk_question where writer = #{id}")
+    int countAllMyQuestion(Member member);
+
+    @Select("select * from mk_question where q_code = #{code}")
+    Question selectOneQuestion(Map<String, Object> boardCode);
+
+    @Update("update mk_question set title = #{title}, question_category = #{questionCategory}, receive_email = #{receiveEmail}, receive_phone = #{receivePhone}, content = #{content}, reg_date = sysdate where q_code = #{qCode}")
+    void updateQuestion(Question question);
+
+    @Insert("insert into mk_question values(concat('qe-', seq_mk_question_no.nextval), #{title}, #{questionCategory}, null, #{writer}, #{phone}, #{email}, #{receiveEmail}, #{receivePhone}, #{content}, default, null, null, null, null)")
+    void insertQuestion(Question question);
+
+    @Delete("delete from mk_question where q_code = #{code}")
+    void deleteMyQuestion(Map<String, Object> boardCode);
 }
