4547eb661df0a04d6086e52de03031c84ef53af8
wkrud <jae6140@naver.com>
2022-03-14 02:10:57 +0900

    question category enum 보수중

--- src/main/java/com/project/market/customerService/model/vo/QuestionCategory.java
+++ src/main/java/com/project/market/customerService/model/vo/QuestionCategory.java
@@ -2,7 +2,7 @@ package com.project.market.customerService.model.vo;
 
 public enum QuestionCategory {
     // 배송지연/불만, 컬리패스, 반품문의, a/s, 환불, 주문결제, 회원정보, 취소, 교환, 상품정보, 기타
-    DE("배송지연/불만"), kurlyPass("kp"), returnProduct("rp"), orderPayment("op"), memberInfo("mi"), cancel("ca"), exchange("ex"), productInfo("pi"), etc("etc");
+    delivery("de"), kurlyPass("kp"), returnProduct("rp"), orderPayment("op"), memberInfo("mi"), cancel("ca"), exchange("ex"), productInfo("pi"), etc("etc");
 
     private String category;
 
@@ -12,7 +12,7 @@ public enum QuestionCategory {
 
     public static QuestionCategory _valueOf(String category){
         switch (category){
-            case "de": return DE;
+            case "de": return delivery;
             case "kp": return kurlyPass;
             case "rp": return returnProduct;
             case "op": return orderPayment;
